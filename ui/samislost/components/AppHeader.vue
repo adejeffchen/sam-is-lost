c92ac0b6cd706baf9383bb1feb8c0cceb6b9b179
sab6794 <86272161+sab6794@users.noreply.github.com>
2021-07-02 12:51:42 -0400

    Create AppHeader.vue
    My changes

---
+++ ui/samislost/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <div class="title"><img src="../assets/Logo.jpg" height=100 width=100 />Sam is Lost</div>
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/createEntry">Create Entry</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+    </ul>
+    <!--
+    <div>
+      <p>
+        <label for="searchTerm">Search</label>
+        <input type="text" size="20" />
+      </p>
+    </div>
+   
+    <ul>
+      <li>
+        <nuxt-link to="/list">List View</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/map">Map View</nuxt-link>
+      </li>
+    </ul>
+    -->
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+import '../assets/style.css'
+</script>
