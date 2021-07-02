974124ccf336b953903017136bdab2756972fdbf
sab6794 <86272161+sab6794@users.noreply.github.com>
2021-07-02 12:51:47 -0400

    Create sighting.vue

---
+++ ui/samislost/components/sighting.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sighting">
+      <div class="sightingTitle">
+        {{ title }}
+      </div>
+      <p>Type: {{ type }}</p>
+      <p>Location: {{ location }}</p>
+      <p class="link"><nuxt-link :to="'list/' + id">See detail ></nuxt-link></p>
+    </div>  
+</template>
+//<a v-bind:href="url">{{ url }}{{ title }} </a>
+<script>
+export default {
+  name: "reportedSighting",
+  props: ["id", "title", "type", "location"],
+};
+</script>,
+
+// data() {
+//   return {
+//     url: "http://localhost:3004/sightings"
+//   }
+// }
+
+
